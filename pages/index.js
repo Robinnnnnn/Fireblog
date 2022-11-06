7ee33909615358f54a03f9d0021c489dac1bde70
Robin Lifshitz <rlifshitz19@gmail.com>
2022-11-05 18:29:58 -0700

    Next Routing Set up

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Link from 'next/link'
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <h1>Hello Fireblog</h1>
+      <Link href="/robinlifshitz"> Link to User Posts </Link>
     </div>
   )
 }
